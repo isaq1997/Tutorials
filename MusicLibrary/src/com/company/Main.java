733dbd763f58688ab16db80360f9dfd8e1401a81
Huseynli Isaq <huseynliir@KAPMAC220-M.local>
2023-04-26 12:23:27 +0400

    lib_2

--- MusicLibrary/src/com/company/Main.java
+++ MusicLibrary/src/com/company/Main.java
@@ -5,17 +5,19 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Song>song1=new ArrayList<>();
+        song1.add(new Song("Truce","Folk"));
         ArrayList<Song>song=new ArrayList<>();
 
         song.add(new Song("Hayway","Rock"));
         song.add(new Song("Gluck","Rock"));
+        song.add(new Song("Teo","Rock"));
         ArrayList<MusicLibrary>mus =new ArrayList<>();
         mus.add(new MusicLibrary("Library_1",song));
+        mus.get(0).addSong(song1.get(0));
 
-        mus.get(0).addSong(song.get(1));
-
-
-        System.out.println(song.get(0).getGenre());
+        System.out.println(mus.get(0).getTitle()+"--->"+song.get(2).getTitle());
+        System.out.println(mus.get(0).get_random_song());
 
     }
 }
